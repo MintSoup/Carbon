d0e8cc9ff225a2034fbb37b81a5a573b10c5f65e
MintSoup <aregak2005@gmail.com>
2020-09-21 20:27:32 +0400

    Added basic AST expression structs

---
+++ inc/ast/expressions.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "token.h"
+typedef enum {
+	ExprBinary,
+	ExprUnary,
+	ExprGrouping,
+	ExprLiteral
+} CarbonExpressionType;
+
+typedef struct {
+	CarbonExpressionType type;
+} CarbonExpression;
+
+typedef struct {
+	CarbonExpression expr;
+
+	CarbonExpression* left;
+	CarbonExpression* right;
+	CarbonToken op;
+} CarbonExprBinary;
+
+typedef struct {
+	CarbonExpression expr;
+
+	CarbonExpression* operand;
+	CarbonToken op;
+} CarbonExprUnary;
+
+typedef struct {
+	CarbonExpression expr;
+
+	CarbonExpression* expression;
+} CarbonExprGrouping;
+
+typedef struct {
+	CarbonExpression expr;
+	CarbonToken token;
+} CarbonExprLiteral;
